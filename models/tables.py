e4e56b07564e4623516556d20d2a7a9fe31bb569
AndyEspejo <aespejo@ucsc.edu>
2016-11-19 12:51:19 -0800

    Added database for calender content not implemented

--- models/tables.py
+++ models/tables.py
@@ -1,19 +1,12 @@
-# Define your tables below (or better in another model file) for example
-#
-# >>> db.define_table('mytable', Field('myfield', 'string'))
-#
-# Fields can be 'string','text','password','integer','double','boolean'
-#       'date','time','datetime','blob','upload', 'reference TABLENAME'
-# There is an implicit 'id integer autoincrement' field
-# Consult manual for more options, validators, etc.
 
 
 
-#db.define_table('dateContent',
- #               """ Field('dateEvent', 'text'),"""
-  #              Field('dateDay', 'int' ),
-   #             Field('dateMonth', 'int'),
-    #            Field('dateYear','int' ),
-        #           )
+
+db.define_table('dateContent',
+                Field('dateEvent', 'text'),
+                Field('dateDay', 'double' ),
+                Field('dateMonth', 'double'),
+                Field('dateYear','double' )
+                   )
 
 
